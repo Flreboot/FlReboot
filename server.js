c197289cafb50a06c4d0b6e264e110e8148e259e
Massimo Pisciotta <massimogamepisciotta@gmail.com>
2025-05-11 22:10:38 +0200

    Fix: eliminata doppia dichiarazione PORT

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const session = require('express-session');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
